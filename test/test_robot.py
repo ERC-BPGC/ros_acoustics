9e40b70ba685a31bac3912374a123a37cc337435
enceladus2000 <tpb2k99@gmail.com>
2021-07-26 08:57:11 +0530

    added RoomPublisher class

--- test/test_robot.py
+++ test/test_robot.py
@@ -1,39 +1,17 @@
 #!/usr/bin/env python3
-from ros_acoustics.robot import Robot
+from ros_acoustics.room_publisher import RoomPublisher
 import rclpy
 from visualization_msgs.msg import Marker
 
 def main(args=None):
 	rclpy.init(args=None)
 
-	# publish_stl('test/data/simple_pipe.stl')
+	room_publisher = RoomPublisher()
+	room_publisher.publish_room()
 
-	robot = Robot()
-	robot.set_pose([1., 0.075, 0.075], [0,0,0])
-	robot.publish_pose()
+	rclpy.spin(room_publisher)
 
-	rclpy.spin(robot)
-
-	robot.destroy_node()
 	rclpy.shutdown()
-
-def publish_stl(path_to_stl):
-	m = Marker()
-	m.pose.position.x = 0
-	m.pose.position.y = 0
-	m.pose.position.z = 0
-	m.pose.orientation.x = 0
-	m.pose.orientation.y = 0
-	m.pose.orientation.z = 0
-	m.pose.orientation.w = 1.0
-	m.type = Marker.MESH_RESOURCE
-	m.scale.x = m.scale.y = m.scale.z = 1.
-	m.action = Marker.ADD
-	m.id = 42
-	m.color.a = 0.6
-	m.color.r = 0.1
-	m.color.g = 0.2
-	m.color.b = 0.75
 
 if __name__ == '__main__':
 	main()
