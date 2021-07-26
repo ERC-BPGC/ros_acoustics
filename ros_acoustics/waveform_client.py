da601564eed411be713d3a3ddccf9a753c394256
enceladus2000 <tpb2k99@gmail.com>
2021-07-26 08:19:14 +0530

    added WaveformClient class

--- ros_acoustics/waveform_client.py
+++ ros_acoustics/waveform_client.py
@@ -1,81 +1,46 @@
 #!/usr/bin/env python3
-import sys
-print(sys.path)
 from ros_acoustics.srv import ComputeWaveforms
 
 import rclpy
 from rclpy.node import Node
 import numpy as np
 import matplotlib.pyplot as plt
-from visualization_msgs.msg import Marker
 
-# class MarkerPublisher(Node):
-# 	def __init__(self, topic):
-# 		super().__init__('minimal_publisher')
-# 		self.publisher_ = self.create_publisher(Marker, topic, 10)
-
-# 	def publish(self, pos):
-# 		marker_msg = Marker
-# 		marker_msg.pose.position.x = pos[0]
-# 		marker_msg.pose.position.x = pos[0]
-# 		marker_msg.pose.position.x = pos[0]
-# 		marker_msg.type = Marker.CUBE
-# 		self.publisher_.publish(marker_msg)
-
-class BasicComputeWaveformsClient(Node):
+class WaveformClient(Node):
+	"""TODO: description"""
+	srv_name = 'compute_waveforms'
 	
 	def __init__(self):
-		super().__init__('basic_waveforms_client')
-		self.cli = self.create_client(ComputeWaveforms, 'compute_waveforms')
+		super().__init__('waveforms_client')
+		self.cli = self.create_client(ComputeWaveforms, self.srv_name)
 		self.get_logger().info('Initiated client.')
 		
 		while not self.cli.wait_for_service(timeout_sec=1.0):
 			self.get_logger().info('Service unavailable, trying again in 1s...')
 		
 		self.req = ComputeWaveforms.Request()
-		self.waveform = np.sin(np.linspace(0, 0.5, 8000))
 
-	def send_request(self, source_pos, mic_pos):
+	def get_waveform(self, source_pos, mic_pos, source_wav):
+		self.send_request(source_pos, mic_pos, source_wav)
+		rclpy.spin_until_future_complete(self, self.future)
+
+		try:
+			response = self.future.result()
+		except Exception as e:
+			self.get_logger().info('Service call failed %r' % (e,))
+			return False
+		else:
+			self.get_logger().info('Received response!')
+			return response.waveform.array
+
+	def send_request(self, source_pos, mic_pos, source_wav):
 		self.req.source_pos.x = source_pos[0]
 		self.req.source_pos.y = source_pos[1]
 		self.req.source_pos.z = source_pos[2]
 		self.req.mic_pos.x = mic_pos[0]
 		self.req.mic_pos.y = mic_pos[1]
 		self.req.mic_pos.z = mic_pos[2]
-		self.req.source_wav.array = list(self.waveform)
+		self.req.source_wav.array = list(source_wav)
 
 		self.future = self.cli.call_async(self.req)
 
-def main(args=None):
-	rclpy.init(args=None)
-
-	waveforms_client = BasicComputeWaveformsClient()
-
-	source_pos = [3., .15, .15]
-	mic_pos = [3.5, .15, .15]
-	waveforms_client.send_request(source_pos, mic_pos)
-
-	while rclpy.ok():
-		rclpy.spin_once(waveforms_client)
-
-		if waveforms_client.future.done():
-			try:
-				response = waveforms_client.future.result()
-			except Exception as e:
-				waveforms_client.get_logger().info(
-					'Service call failed %r' % (e,))
-			else:
-				waveforms_client.get_logger().info('Received response! Plotting...')
-
-				plt.plot(response.waveform.array)
-				plt.show()
-
-			break
-
-	print('Exiting...')
-	waveforms_client.destroy_node()
-
-	rclpy.shutdown()
-
-if __name__ == "__main__":
-	main()
--- test/test_robot.py
+++ test/test_robot.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 from ros_acoustics.robot import Robot
 import rclpy
+from visualization_msgs.msg import Marker
 
 def main(args=None):
 	rclpy.init(args=None)
+
+	# publish_stl('test/data/simple_pipe.stl')
 
 	robot = Robot()
 	robot.set_pose([1., 0.075, 0.075], [0,0,0])
@@ -13,6 +16,24 @@ def main(args=None):
 
 	robot.destroy_node()
 	rclpy.shutdown()
+
+def publish_stl(path_to_stl):
+	m = Marker()
+	m.pose.position.x = 0
+	m.pose.position.y = 0
+	m.pose.position.z = 0
+	m.pose.orientation.x = 0
+	m.pose.orientation.y = 0
+	m.pose.orientation.z = 0
+	m.pose.orientation.w = 1.0
+	m.type = Marker.MESH_RESOURCE
+	m.scale.x = m.scale.y = m.scale.z = 1.
+	m.action = Marker.ADD
+	m.id = 42
+	m.color.a = 0.6
+	m.color.r = 0.1
+	m.color.g = 0.2
+	m.color.b = 0.75
 
 if __name__ == '__main__':
 	main()
